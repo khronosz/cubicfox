cae82515d0542b3fc2dbb4df00fc8f576e21a7c5
khronosz <chronosgeneral@gmail.com>
2022-05-05 20:57:54 +0200

    Initial commit

---
+++ src/main/java/com/example/cubicfox/CubicfoxApplication.java
@@ -0,0 +1,12 @@
+package com.example.cubicfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubicfoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubicfoxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/cubicfox/CubicfoxApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.cubicfox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubicfoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
